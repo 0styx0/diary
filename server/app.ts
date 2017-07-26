4cd255389be06aae3421d411207079789cf97aa7
DovidM <landyx47@gmail.com>
2017-07-25 21:49:57 -0400

    Allowed client to connect

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,22 @@ import schema from './src/schema';
 
 const app = express();
 
+
+
+app.use(function(req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET");
+
+  if (req.method === 'OPTIONS') {
+      res.status(200);
+      return res.end();
+  }
+  next()
+});
+
 app.use('/api', graphsqlHTTP({
     schema,
     graphiql: true
