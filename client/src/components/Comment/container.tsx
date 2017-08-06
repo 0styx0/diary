9b0fec6924c71e3cc946d04827ac4e3136dab161
DovidM <landyx47@gmail.com>
2017-08-06 14:52:19 -0400

    Remove deleted comments from cache

--- client/src/components/Comment/container.tsx
+++ client/src/components/Comment/container.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { graphql } from 'react-apollo';
 import { TextPostCommentDeletion } from '../../graphql/textPosts/comment';
+import { TextPostQuery } from '../../graphql/textPosts';
 import Comment from './';
 
 interface Props {
@@ -20,6 +21,14 @@ class CommentContainer extends React.Component<Props, {}> {
             variables: {
                 commentId: this.props.id,
                 postId: this.props.postId
+            },
+            update: (store) => {
+
+                const storage = store.readQuery({query: TextPostQuery}); // if post type is already in the cache
+
+                storage.textPosts = storage.textPosts.filter(post => post.id !== this.props.postId);
+
+                store.writeQuery({query: TextPostQuery, data: storage });
             }
         });
     }
